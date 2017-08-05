9283972e4d22be9ae7b07f713d2ebd8c2014b801
hernxntk <hernxntk@gmail.com>
2017-08-05 08:32:50 -0500

    nuevo commi

---
+++ slots/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <mostrar-formulario>
+        <div slot="titulo">
+          <h3>Cambiar Contraseña</h3>
+        </div>
+        <div slot="elementos">
+          <div class="form-group">
+            <label for="correo">Correo Electrónico</label>
+            <input id="correo" type="email"
+              class="form-control"
+              placeholder="Escribe tu Correo Electrónico"
+            >
+          </div>
+          <div class="form-group">
+            <label for="clave">Contraseña</label>
+            <input id="clave" type="password"
+              class="form-control"
+              placeholder="Escribe tu Contraseña"
+            >
+          </div>
+        </div>
+        <div slot="boton">
+          <button class="btn btn-primary">Iniciar Sesión</button>
+        </div>
+      </mostrar-formulario>
+    </div>
+  </div>
+</template>
+
+<script>
+  import MostrarFormulario from './mostrarFormulario.vue'
+
+  export default {
+    components: {
+      mostrarFormulario: MostrarFormulario
+    },
+    data(){
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ slots/src/mostrarFormulario.vue
@@ -0,0 +1,34 @@
+<template>
+  <form class="form">
+    <slot name="titulo"></slot>
+    <slot name="elementos"></slot>
+    <slot name="boton"></slot>
+    <!--
+    <div class="form-group">
+      <label for="correo">Correo Electrónico</label>
+      <input id="correo" type="email"
+        class="form-control"
+        placeholder="Escribe tu Correo Electrónico"
+      >
+    </div>
+    <div class="form-group">
+      <label for="clave">Contraseña</label>
+      <input id="clave" type="password"
+        class="form-control"
+        placeholder="Escribe tu Contraseña"
+      >
+    </div>
+    <button class="btn btn-primary">Iniciar Sesión</button>
+  -->
+  </form>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
